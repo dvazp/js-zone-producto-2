95d1618347117b7018c383b7e4072923134e8d68
MarcCorcolesUOC <mcorcoles@uoc.edu>
2025-11-07 18:18:47 +0100

    Añadido LocalStorage a almacenaje y corregido errores login con localStorage

--- js/almacenaje.js
+++ js/almacenaje.js
@@ -1,1 +1,6 @@
 //Para las operaciones CRUD comunes en todas las paginas
+
+export function obtenerUsuarioActivo() { // Obtiene el Usuario Activo y lo Devuelve.
+    const usuarioActivo = localStorage.getItem("UsuarioActivo");
+    return usuarioActivo;
+}
--- js/login.js
+++ js/login.js
@@ -1,5 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Registra el manejador del evento submit del formulario (con guard)
+    formLogin?.addEventListener("submit", LoginUser); 
+    // Mostramos el usuario activo
+    mostrarUsuarioActivo();
+})
 // Importa la lista de usuarios simulados desde datos.js
-import {usuarios} from './datos.js';
+import { usuarios } from './datos.js';
+import { obtenerUsuarioActivo } from './almacenaje.js';
 
 // Índice por nombre de usuario para búsqueda O(1)
 const usuariosByName = new Map(usuarios.map(u => [u.email, u]));
@@ -10,8 +17,6 @@ const correo = document.getElementById("user");
 const password = document.getElementById("password");
 const userHeader = document.getElementById("user_header");
 
-// Registra el manejador del evento submit del formulario (con guard)
-formLogin?.addEventListener("submit", LoginUser);
 
 /**
  * Manejador del envío del formulario de login.
@@ -45,10 +50,16 @@ function LoginUser(event){
 
     // Credenciales correctas
     alert("Inicio de sesión correcto");
-    localStorage.setItem(UsuarioActivo,correo.value) // Guardamos en localStorage el correo del usuario cuando hace login.
-
+    localStorage.setItem("UsuarioActivo", correo.value) // Guardamos en localStorage el correo del usuario cuando hace login.
 }
 
+// Funcion que MuestraUsuarioActivo
 function mostrarUsuarioActivo() {
-
+    let usuarioActivo = obtenerUsuarioActivo();
+    if (usuarioActivo) {
+        userHeader.textContent = usuarioActivo;
+    } else {
+        userHeader.textContent = "No Login";
+    }
 }
+
