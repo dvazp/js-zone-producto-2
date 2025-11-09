96e285c432b242bb864c3f115707ca6c0a616954
MarcCorcolesUOC <mcorcoles@uoc.edu>
2025-11-09 18:34:18 +0100

    Arreglado Login y añadido que aparezca en todas las paginas

--- js/almacenaje.js
+++ js/almacenaje.js
@@ -4,3 +4,8 @@ export function obtenerUsuarioActivo() { // Obtiene el Usuario Activo y lo Devue
     const usuarioActivo = localStorage.getItem("UsuarioActivo");
     return usuarioActivo;
 }
+
+// Funcion que guarda el usuario logueado como usuario activo.
+export function loguearUsuario(usuario) {
+    localStorage.setItem("UsuarioActivo", usuario);
+}
